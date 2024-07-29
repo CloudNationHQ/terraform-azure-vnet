19ea4553a2823afc08c069a51981b57f5b2e85b9
Tobias <tobias.vanstokkum@cloudnation.nl>
2024-07-29 12:26:22 +0200

    feat: add network security group output

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "subnets" {
   value = azurerm_subnet.subnets
 }
 
+output "nsg" {
+  value = azurerm_network_security_group.nsg
+}
+
 output "subscriptionId" {
   value = data.azurerm_subscription.current.subscription_id
 }
